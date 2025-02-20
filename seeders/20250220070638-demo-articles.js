3972307725fe8c6a159b20f19ac74623586ffd0a
Angela Roosmägi <angela.roosmagi@voco.ee>
2025-02-20 09:18:30 +0200

    Created article table and added table data

---
+++ seeders/20250220070638-demo-articles.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up (queryInterface, Sequelize) {
+    return Promise.all([ 
+      queryInterface.bulkDelete('Articles', null, {}),
+      queryInterface.bulkInsert('Articles', [{ 
+        name: 'Introduction to Ashtanga', 
+        slug: 'introduction-to-ashtanga', 
+        image: 'ashtanga.jpg', 
+        body: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lorem ex, blandit vel lectus a, egestas hendrerit ex. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos. </p>',
+        published: new Date('2020-01-08 15:02:30'), 
+        createdAt: new Date(), 
+        updatedAt: new Date() 
+      }]), 
+      queryInterface.bulkInsert('Articles', [{ 
+        name: 'Morning vinyasa flow routine', 
+        slug: 'morning-vinyasa-flow-routine', 
+        image: 'morning.jpg', 
+        body: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lorem ex, blandit vel lectus a, egestas hendrerit ex. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos. </p>',
+        published: new Date('2020-04-14 15:02:41'), 
+        createdAt: new Date(), 
+        updatedAt: new Date() 
+      }]), 
+      queryInterface.bulkInsert('Articles', [{ 
+        name: 'Secrets of a yoga teacher', 
+        slug: 'secrets-of-a-yoga-teacher', 
+        image: 'yoga-teacher.jpg',
+        body: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lorem ex, blandit vel lectus a, egestas hendrerit ex. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos. </p>',
+        published: new Date('2020-05-28 15:02:55'), 
+        createdAt: new Date(), 
+        updatedAt: new Date() 
+      }]), 
+    ])
+  },
+
+  down (queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('Articles', null, {})
+  }
+};
